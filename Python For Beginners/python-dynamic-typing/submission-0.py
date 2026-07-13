30426aaa125afdf4f6b9a9c1e937678655484d1b
manav1337 <manavsadh123@gmail.com>
2026-07-13 18:09:41 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 0
+print(type(variable))
+variable = 1.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "dsad"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
